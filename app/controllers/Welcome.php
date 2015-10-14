84bfb36cc5acea0b38b61d1711a442470fe31c84
tak0294 <tak0294@gmail.com>
2015-10-14 19:55:38 +0900

    初回コミット

---
+++ app/controllers/Welcome.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->database();
+		$this->load->library("FluentPDO/FluentPDO", array("pdo"=>$this->db->conn_id));
+		//$query = $this->fluentpdo->from('M_admin')->where('admin_id = ?', 'admin')->getQuery();
+		$statement = $this->fluentpdo->from('M_admin')->where(array("admin_id LIKE" => "%dmin%"))->order("admin_id desc");
+		
+		$param = $statement->getParameters();
+		$sql   = $statement->getQuery();
+		echo $sql;
+		print_r($param);
+		$res = $this->db->query($sql, $param)->result();
+		print_r($res);
+		//foreach($query as $row) {
+			//print_r($row);
+		//}
+
+		$this->load->view('welcome_message');
+	}
+}
